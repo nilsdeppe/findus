4bec4fa7036d3da2971cbc6867383c5ae437599d
Nils Deppe <nils@nilsdeppe.com>
2024-12-03 13:36:10 -0800

    Add IsCollection type traits

---
+++ Rts/IsCollection.hpp
@@ -0,0 +1,25 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <type_traits>
+
+namespace rts {
+/// \cond
+template <class ParallelComponent>
+class DistributedObjectCollection;
+/// \endcond
+
+/// Inherits from `std::true_type` if `ParallelComponent` is a
+/// `DistributedObjectCollection`
+template <typename ParallelComponent>
+struct is_collection
+    : std::is_base_of<DistributedObjectCollection<ParallelComponent>,
+                      ParallelComponent> {};
+
+/// Is `true` if  `ParallelComponent` is a `DistributedObjectCollection`
+template <typename ParallelComponent>
+static constexpr bool is_collection_v = is_collection<ParallelComponent>::value;
+}  // namespace rts
