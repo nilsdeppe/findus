56444a86c9b3fe0c819c0d86d3bb003baa1021c8
Nils Deppe <nils@nilsdeppe.com>
2025-06-05 07:11:48 -0700

    Use offset in MessageHeader and include src/dest ranks

--- Rts/MessageHeader.hpp
+++ Rts/MessageHeader.hpp
@@ -33,11 +33,18 @@ namespace rts {
  *   function `set_data_was_serialized()` must be used to set the flag for
  *   that data was serialized. Not setting the flag is undefined behavior with
  *   no method of diagnosing it.
+ *   Note: the `number_of_bytes_in_message` is the total bytes in the message,
+ *   counting both the `MessageHeader` and the serialized data.
  * - `distributed_object_index` is the index of the distributed object (i.e.,
  *   a class type). While it could be any desired index type, hashing a
  *   human-readable name of the distributed object class is recommended.
- * - `data` is a pointer to the byte stream of data, which may or
- *   may not have been serialized.
+ * - `data_offset` is the number of bytes from the message's address to the
+ *   byte stream of data, which may or may not have been serialized. The
+ *   functions `rts::create_data_in_message()` and `rts::data_from_message()`
+ *   should be used to access the data rather than doing the calculations
+ *   directly.
+ * - `source_rank` is the index of the MPI rank that is sending the message.
+ * - `destination_rank` is the index of the MPI that is receiving the message.
  *
  * See
  * - `rts::number_of_bytes_in_message()`
@@ -45,13 +52,18 @@ namespace rts {
  * - `rts::set_data_was_serialized()`
  * - `rts::data_from_message()`
  * - `rts::MessageHeader::no_collection_index()`
+ * - `rts::data_location()`
+ * - `rts::create_data_in_message()`
+ * - `rts::data_from_message()`
  */
 struct alignas(64) MessageHeader {
   detail::MemberFunctionPtr member_function_ptr = {};
   std::uint64_t target_collection_index = 0;
   std::uint64_t number_of_bytes_in_message = 0;
   std::uint32_t distributed_object_index = 0;
-  char* data = nullptr;
+  std::uint32_t data_offset = 0;
+  std::int32_t source_rank = -1;
+  std::int32_t destination_rank = -1;
 
   /// \brief The value of `target_collection_index` used when the distributed
   /// object is not a collection.
@@ -60,7 +72,12 @@ struct alignas(64) MessageHeader {
   }
 };
 
-/// \brief Get the number of bytes in the message (`data`)
+/*!
+ * \brief Get the number of bytes in the message.
+ *
+ * Note: this is the total bytes in the message, counting the
+ * `MessageHeader`, padding, and the serialized data.
+ */
 inline std::uint64_t number_of_bytes_in_message(const MessageHeader& message) {
   return std::uint64_t{std::numeric_limits<std::uint64_t>::max() >> 1} bitand
          message.number_of_bytes_in_message;
@@ -90,6 +107,44 @@ inline void set_data_was_serialized(MessageHeader& message,
 }
 
 /*!
+ * \brief Returns the address of the data/byte stream in a message.
+ */
+inline char* data_location(MessageHeader& message_header) {
+  return reinterpret_cast<char*>(&message_header) + message_header.data_offset;
+}
+
+/*!
+ * \brief Creates the `DataTypeReturned` in the data portion of the message.
+ *
+ * This is used for intranode data where we can in-place construct a
+ * `std::tuple` of the arguments in the message and then move the arguments
+ * into the tuple. It can also be used for internode messages where all the
+ * types are trivially copyable under the assumption that trivially copyable
+ * means the type can be copied without having to worry about pointers
+ * becoming in correct to heap data.
+ */
+template <class DataTypeReturned>
+auto create_data_in_message(MessageHeader& message_header)
+    -> DataTypeReturned* {
+  if ((reinterpret_cast<std::uintptr_t>(data_location(message_header)) %
+       alignof(DataTypeReturned)) != 0) {
+    throw std::runtime_error{
+        "Unable to convert data at address to the requested type because the "
+        "alignment of the requested type is stricter than the alignment of the "
+        "data. This means it is not possible to do a safe conversion or to "
+        "have the data be interpreted as the requested type. The alignment of "
+        "the type is " +
+        std::to_string(alignof(DataTypeReturned)) +
+        std::string{" and the memory address is " +
+                    std::to_string(reinterpret_cast<std::uintptr_t>(
+                        data_location(message_header)))}};
+  }
+  DataTypeReturned* data =
+      new (rts::data_location(message_header)) DataTypeReturned{};
+  return data;
+}
+
+/*!
  * \brief Converts the internal data pointer from the message header to the
  * desired type.
  *
@@ -101,7 +156,7 @@ inline void set_data_was_serialized(MessageHeader& message,
  */
 template <class DataTypeReturned>
 auto data_from_message(MessageHeader& message_header) -> DataTypeReturned* {
-  if ((reinterpret_cast<std::uintptr_t>(message_header.data) %
+  if ((reinterpret_cast<std::uintptr_t>(data_location(message_header)) %
        alignof(DataTypeReturned)) != 0) {
     throw std::runtime_error{
         "Unable to convert data at address to the requested type because the "
@@ -112,8 +167,8 @@ auto data_from_message(MessageHeader& message_header) -> DataTypeReturned* {
         std::to_string(alignof(DataTypeReturned)) +
         std::string{" and the memory address is " +
                     std::to_string(reinterpret_cast<std::uintptr_t>(
-                        message_header.data))}};
+                        data_location(message_header)))}};
   }
-  return reinterpret_cast<DataTypeReturned*>(message_header.data);
+  return reinterpret_cast<DataTypeReturned*>(data_location(message_header));
 }
 }  // namespace rts
