cbca44cfc02f32648651f3df10ac629e55063a63
Nils Deppe <nils@nilsdeppe.com>
2025-06-11 18:41:16 -0700

    Add quiescence detection exception

---
+++ Rts/QdException.cpp
@@ -0,0 +1,11 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Rts/QdException.hpp"
+
+#include <string>
+
+namespace rts {
+QdException::QdException(const std::string& message) : Exception(message) {}
+}  // namespace rts
