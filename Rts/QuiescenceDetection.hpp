1fdfe5e62820fd523fe432c20e62569eb4b255f4
Nils Deppe <nils@nilsdeppe.com>
2025-06-11 19:48:15 -0700

    Add global quiescence detection algorithm

--- Rts/QuiescenceDetection.hpp
+++ Rts/QuiescenceDetection.hpp
@@ -7,15 +7,21 @@
 #include <atomic>
 #include <cstdint>
 #include <limits>
+#include <map>
+#include <mpi.h>
 #include <new>
+#include <optional>
 #include <stdexcept>
 #include <string>
+#include <vector>
+
+#include "Rts/HardwareInfo.hpp"
 
 namespace rts {
 /// Quiescence detection constructs
 namespace qd {
 /*!
- * \brief The data needed for local quiescence detection within an MPI rank.
+ * \brief The data needed for local quiescence detection within a process.
  *
  * This is used for implementing the SKR algorithm used by Charm++ and
  * described in:
@@ -108,8 +114,8 @@ class Local {
 };
 
 /*!
- * \brief The data needed for global quiescence detection across all MPI
- * ranks.
+ * \brief The data needed for global quiescence detection across all
+ * processes.
  *
  * The algorithm implemented for global quiescence detection is the BCJ
  * algorithm of:
@@ -121,9 +127,9 @@ class Local {
  *   DOI: 10.1080/00036810600595813
  *
  * In BJC a sweep counter is used to keep track of different quiescence
- * detection attempts. A virtual spanning tree is used across the MPI ranks
- * where each rank has $N$ children. Ranks with no children are referred to
- * leaf nodes in the tree and the single rank with no parent is called the
+ * detection attempts. A virtual spanning tree is used across the processes
+ * where each process has $N$ children. Processes with no children are referred
+ * to leaf nodes in the tree and the single process with no parent is called the
  * root. The algorithm has a down traversal that starts at the root node and
  * an up traversal that starts at the leaf nodes. A new down traversal is only
  * started after an up traversal completes. Note that down and up traversals
@@ -215,7 +221,8 @@ class Local {
  * means multiple sweeps could be simultaneously in progress. This is by
  * design and allows for termination as early as possible.
  */
-struct Global {
+class Global {
+ public:
   struct Message {
     std::uint64_t direction_and_sweep_number{0};
     std::int64_t count{0};
@@ -240,6 +247,11 @@ struct Global {
     static bool down_traversal(const Message& message) {
       return not up_traversal(message);
     }
+
+    static std::uint64_t sweep_number(const Message& message) {
+      // Zero highest bit by shifting it left and then right.
+      return ((message.direction_and_sweep_number << 1) >> 1);
+    }
   };
 
   Global();
@@ -249,18 +261,133 @@ struct Global {
   Global& operator=(Global&&);
   ~Global();
 
-  Global(int my_rank, int total_ranks);
+  Global(int my_process, int total_processs, size_t max_simultaneous_qds);
 
-  std::uint64_t local_sweep_number{0};
-  std::uint64_t last_regular_message_sweep_number{0};
-  std::int64_t local_sends{0};
-  std::int64_t local_processed{0};
+  /// \brief Returns `true` if this global QD state is for a leaf process.
+  bool leaf() const;
+
+  /// \brief Returns `true` if this global QD state is for the root process.
+  bool root() const;
+
+  /// \brief Resets the state to start another quiescence detection.
+  void reset();
+
+  /// \brief Resets the state to start another quiescence detection, first
+  /// verifying we completed a QD first.
+  void safe_reset();
+
+  /// \brief Returns `true` if quiescence is detected and `false` if not.
+  ///
+  /// This runs the global QD algorithm, including the MPI communication.
+  ///
+  /// After this function completes, you should call `wait_for_broadcast()` to
+  /// ensure the termination broadcast for QD has completed. This separation is
+  /// so that a separate thread can be used as a timeout for the broadcast
+  /// completion.
+  bool check(MPI_Comm& comm);
+
+  /// \brief Blocks until the children have received the termination broadcast.
+  ///
+  /// In addition to waiting for all the termination broadcasts to complete,
+  /// it also verifies that all down and up messages are complete.
+  void wait_for_broadcast(MPI_Comm& comm);
+
+  /// \brief Should be called every time a regular message is received with
+  /// sweep number in that regular message.
+  void update_last_regular_message_sweep_number(
+      std::uint64_t sweep_number_from_regular_message);
+
+  /// \brief Increments the number of sent regular messages by one.
+  void increment_sends();
+
+  /// \brief Increments the number of processed regular messages by one.
+  void increment_processed();
+
+  /// \brief Returns the local sweep number.
+  std::uint64_t local_sweep_number() const { return local_sweep_number_; }
+
+ private:
+  // Data structure used for keeping track of the down messages. Since we do
+  // non-blocking sends we need to keep the Message around until both children
+  // have received the data. We store the non-blocking send requests as well,
+  // though the send requests are only valid if we have a child in that
+  // direction (left or right). Checking for a child must be done separately.
+  struct DownData {
+    Message message{};
+    MPI_Request child_left_request{};
+    MPI_Request child_right_request{};
+  };
+
+  // Data structure used for keeping track of the up messages. Since we do
+  // a non-blocking send we need to keep the Message around until the parent
+  // has received the data. We store the non-blocking send request as well.
+  struct UpData {
+    Message message{};
+    MPI_Request parent_request{};
+  };
+
+  // Data structure used for tracking the total number of inter-process sends
+  // and receives, as well as the number of children a particular process has
+  // received from.
+  struct AccumulationData {
+    std::int64_t send_minus_processed{0};
+    std::uint8_t number_of_children_received_from{0};
+  };
+
+  /// \brief Performs the down traversal algorithm.
+  void down_traversal(MPI_Comm& comm);
+
+  /// \brief Sends the down message to the children and sets the requests.
+  void send_down_to_children(MPI_Comm& comm, DownData& down_data);
+
+  /// \brief Cleans up down messages that have sent successfully.
+  void cleanup_down_messages();
+
+  /// \brief Performs the down traversal algorithm and calls the down
+  /// traversal algorithm if there is no up traversal to be done.
+  void up_traversal(MPI_Comm& comm);
+
+  /// \brief Receives all pending up messages from `recv_process`.
+  void receive_up_messages_from(MPI_Comm& comm, int recv_process);
+
+  /// \brief Cleans up up messages that have sent successfully.
+  void cleanup_up_messages();
+
+  /// \brief Notifies the children that quiescence has been reached.
+  ///
+  /// This intentionally does not do an MPI broadcast so that the
+  /// communication can be done with its own tag and not interfere with any
+  /// other communication.
+  void send_quiescence_broadcast_to(
+      MPI_Comm& comm, std::optional<MPI_Request>& broadcast_child_request,
+      int child_process);
+
+  std::uint64_t local_sweep_number_{0};
+  std::uint64_t last_regular_message_sweep_number_{0};
+  std::int64_t local_sends_{0};
+  std::int64_t local_processed_{0};
   // The sentinel value of `-1` denotes "not filled" while the sentinel value
   // `std::numeric_limits<int>::min()` denotes "not initialized".
-  int self_rank{std::numeric_limits<int>::min()};
-  int parent_rank{std::numeric_limits<int>::min()};
-  int child_left_rank{std::numeric_limits<int>::min()};
-  int child_right_rank{std::numeric_limits<int>::min()};
+  int self_process_{std::numeric_limits<int>::min()};
+  int parent_process_{std::numeric_limits<int>::min()};
+  int child_left_process_{std::numeric_limits<int>::min()};
+  int child_right_process_{std::numeric_limits<int>::min()};
+
+  // Holds all of the sent but not completed down messages.
+  std::vector<DownData> down_messages_{};
+  size_t max_simultaneous_qds_{0};
+  std::uint64_t root_down_sweep_{0};
+  // Holds all of the sent but not completed up messages.
+  std::vector<UpData> up_messages_{};
+  // Holds all the accumulated counts from children.
+  //
+  // The key is the sweep number
+  std::map<std::uint64_t, AccumulationData> accum_data_{};
+  // The terminate flag sent at QD. We need some persistent data to allow us
+  // to catch hangs.
+  std::int32_t terminate_{0};
+  std::optional<MPI_Request> broadcast_left_child_request_{std::nullopt};
+  std::optional<MPI_Request> broadcast_right_child_request_{std::nullopt};
 };
 }  // namespace qd
 }  // namespace rts
