e9732d7970eb40e256b63c6f483ce1d41c21c893
Nils Deppe <nils@nilsdeppe.com>
2025-06-17 11:10:00 -0700

    Add CMake file for setting up flags

---
+++ cmake/RtsFlags.cmake
@@ -0,0 +1,45 @@
+# Copyright, Nils Deppe, 2022-
+# Distributed under the MIT License.
+# See LICENSE.txt for details.
+
+set(_RTS_CXX_FLAGS
+  "-W;\
+-Wall;\
+-Wcast-align;\
+-Wcast-qual;\
+-Wdisabled-optimization;\
+-Wdocumentation;\
+-Wextra;\
+-Wformat-nonliteral;\
+-Wformat-security;\
+-Wformat-y2k;\
+-Wformat=2;\
+-Winvalid-pch;\
+-Wmissing-declarations;\
+-Wmissing-field-initializers;\
+-Wmissing-format-attribute;\
+-Wmissing-include-dirs;\
+-Wmissing-noreturn;\
+-Wnewline-eof;\
+-Wnon-virtual-dtor;\
+-Wold-style-cast;\
+-Woverloaded-virtual;\
+-Wpacked;\
+-Wpedantic;\
+-Wpointer-arith;\
+-Wredundant-decls;\
+-Wshadow;\
+-Wsign-conversion;\
+-Wstack-protector;\
+-Wswitch-default;\
+-Wunreachable-code;\
+-Wwrite-strings")
+
+add_library(RtsFlags INTERFACE)
+
+foreach(_FLAG ${_RTS_CXX_FLAGS})
+  set_property(TARGET RtsFlags
+    APPEND PROPERTY
+    INTERFACE_COMPILE_OPTIONS
+    "$<$<COMPILE_LANGUAGE:CXX>:${_FLAG}>")
+endforeach()
