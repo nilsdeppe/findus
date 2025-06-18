16ca9399ad47021827b681f11d3598b124fb99f0
Nils Deppe <nils@nilsdeppe.com>
2025-06-18 07:48:38 -0700

    Add support for using doctest in ToyRTS library

---
+++ Rts/DoctestImpl.cpp
@@ -0,0 +1,11 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#if defined(RTS_ENABLE_TESTING)
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.h"
+#else
+static_assert(
+    false, "Should never compile DoctestImpl.cpp when testing isn't enabled.");
+#endif
