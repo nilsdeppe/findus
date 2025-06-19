224b9ade28027a2d44fb993e4d66f7f4d21b7100
Nils Deppe <nils@nilsdeppe.com>
2025-06-19 09:19:23 -0700

    Add tests for exceptions

--- Rts/Exceptions/Exception.cpp
+++ Rts/Exceptions/Exception.cpp
@@ -4,10 +4,40 @@
 
 #include "Rts/Exceptions/Exception.hpp"
 
+#include <exception>
 #include <stdexcept>
 #include <string>
 
 namespace rts {
 Exception::Exception(const std::string& message)
     : std::runtime_error(message) {}
+
+static_assert(std::is_base_of_v<std::runtime_error, Exception>);
 }  // namespace rts
+
+#if defined(RTS_ENABLE_TESTING)
+
+#include <doctest/doctest.h>
+#include <string>
+#include <type_traits>
+
+namespace rts {
+TEST_CASE("Exception") {
+  try {
+    throw Exception("Throwing test exception 0.");
+  } catch (const Exception& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception 0.");
+  }
+  try {
+    throw Exception("Throwing test exception 1.");
+  } catch (const std::runtime_error& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception 1.");
+  }
+  try {
+    throw Exception("Throwing test exception 2.");
+  } catch (const std::exception& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception 2.");
+  }
+}
+}  // namespace rts
+#endif
--- Rts/Exceptions/Mpi.cpp
+++ Rts/Exceptions/Mpi.cpp
@@ -4,8 +4,44 @@
 
 #include "Rts/Exceptions/Mpi.hpp"
 
+#include <exception>
 #include <string>
 
 namespace rts {
 MpiException::MpiException(const std::string& message) : Exception(message) {}
+
+static_assert(std::is_base_of_v<Exception, MpiException>);
+static_assert(std::is_base_of_v<std::runtime_error, MpiException>);
 }  // namespace rts
+
+#if defined(RTS_ENABLE_TESTING)
+
+#include <doctest/doctest.h>
+#include <string>
+#include <type_traits>
+
+namespace rts {
+TEST_CASE("MpiException") {
+  try {
+    throw MpiException("Throwing test exception -0.");
+  } catch (const MpiException& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception -0.");
+  }
+  try {
+    throw MpiException("Throwing test exception 0.");
+  } catch (const Exception& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception 0.");
+  }
+  try {
+    throw MpiException("Throwing test exception 1.");
+  } catch (const std::runtime_error& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception 1.");
+  }
+  try {
+    throw MpiException("Throwing test exception 2.");
+  } catch (const std::exception& e) {
+    CHECK(std::string{e.what()} == "Throwing test exception 2.");
+  }
+}
+}  // namespace rts
+#endif
