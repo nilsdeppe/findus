10d17eee1ce7e161d74c93552ed140f6625dab1d
Nils Deppe <nils@nilsdeppe.com>
2025-06-19 13:22:21 -0700

    Add test for ActionState

--- Rts/ActionState.cpp
+++ Rts/ActionState.cpp
@@ -6,9 +6,10 @@
 
 #include <ostream>
 #include <stdexcept>
+#include <type_traits>
 
 namespace rts {
-std::ostream& operator<<(std::ostream& os, const ActionState& action_state) {
+std::ostream& operator<<(std::ostream& os, const ActionState action_state) {
   switch (action_state) {
     case ActionState::Uninitialized:
       return os << "Uninitialized";
@@ -16,8 +17,30 @@ std::ostream& operator<<(std::ostream& os, const ActionState& action_state) {
       return os << "Success";
     case ActionState::Resubmit:
       return os << "Resubmit";
+    case ActionState::End:
+      return os << "End";
     default:
-      throw std::runtime_error("Invalid action state");
+      return os << "unknown_action_state("
+                << static_cast<std::uint32_t>(action_state) << ")";
   }
 }
 }  // namespace rts
+
+#if defined(RTS_ENABLE_TESTING)
+
+#include <doctest/doctest.h>
+#include <string>
+
+#include "Rts/Detail/GetOutput.hpp"
+
+namespace rts {
+TEST_CASE("ActionState") {
+  CHECK(detail::get_output(ActionState::Uninitialized) == "Uninitialized");
+  CHECK(detail::get_output(ActionState::Success) == "Success");
+  CHECK(detail::get_output(ActionState::Resubmit) == "Resubmit");
+  CHECK(detail::get_output(ActionState::End) == "End");
+  CHECK(detail::get_output(static_cast<ActionState>(128)) ==
+        "unknown_action_state(128)");
+}
+}  // namespace rts
+#endif
