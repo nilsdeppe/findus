c121a423d1dc9d66c1ad7c00b20fe74ddf6d8b23
Nils Deppe <nils@nilsdeppe.com>
2025-06-19 13:54:09 -0700

    Clean up DistributedObjectBase code

---
+++ Rts/Detail/DistributedObjectBase.hpp
@@ -0,0 +1,20 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace rts::detail {
+/*!
+ * \brief The internally used type-erased base class for distributed objects.
+ *
+ * This should never get exposed to user code since they will always be
+ * querying their most derived distributed objects, and internally we generate
+ * template functions to do the invocation. This allows us to preserve the
+ * type information across the network.
+ */
+class DistributedObjectBase {
+ public:
+  virtual ~DistributedObjectBase() = 0;
+};
+}  // namespace rts::detail
