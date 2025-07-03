ab6876e1b8a5266340f74dd770f58fc4d7a94377
Nils Deppe <nils@nilsdeppe.com>
2025-07-03 08:53:01 -0700

    Add internal vector stream operator

---
+++ Rts/Detail/VectorStream.hpp
@@ -0,0 +1,26 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <ostream>
+#include <vector>
+
+namespace rts::detail {
+/// \brief Internally used stream operator for std::vector.
+///
+/// Intentionally not in the global namespace and should be used explicitly in
+/// order to minimize collision with user code.
+template <typename T, typename A>
+std::ostream& operator<<(std::ostream& os, const std::vector<T, A>& v) {
+  os << "[";
+  for (size_t i = 0; i < v.size(); ++i) {
+    os << v[i];
+    if (i != v.size() - 1)
+      os << ", ";
+  }
+  os << "]";
+  return os;
+}
+}  // namespace rts::detail
