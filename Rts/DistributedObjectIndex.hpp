b24eeef82d38ec1779366285585f7625990ae703
Nils Deppe <nils@nilsdeppe.com>
2025-09-05 07:35:40 -0700

    Move distributed_object_index() into its own file
    This allows us to call it from multiple locations

---
+++ Rts/DistributedObjectIndex.hpp
@@ -0,0 +1,26 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <cstdint>
+
+namespace rts::detail {
+/*!
+ * \brief Counter used to assign each distributed object a unique integer ID.
+ *
+ * The function `detail::distributed_object_index()` gives the resulting index
+ * for a parallel component.
+ */
+extern uint32_t distributed_object_index_counter;
+
+/*!
+ * \brief Returns the unique ID for the parallel component.
+ */
+template <typename ParallelComponent>
+uint32_t distributed_object_index() {
+  static uint32_t index = (distributed_object_index_counter++);
+  return index;
+}
+}  // namespace rts::detail
