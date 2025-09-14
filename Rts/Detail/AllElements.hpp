a53fa20f205828688abbdee01fa4a2903f837b6d
Nils Deppe <nils@nilsdeppe.com>
2025-09-13 19:55:33 -0700

    Add reduction::AllElements

---
+++ Rts/Detail/AllElements.hpp
@@ -0,0 +1,19 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+namespace rts::reduction::detail {
+/// \brief Predicate that always returns `true`.
+///
+/// Used internally for `reduction()`, which calls
+/// `reduction_over(AllElements{})`. Note that `reduction_over()` optimizes
+/// for the case where `AllElements` is passed.
+struct AllElements {
+  template <class T>
+  bool operator()(const T /*unused_id*/) const {
+    return true;
+  }
+};
+}  // namespace rts::reduction::detail
