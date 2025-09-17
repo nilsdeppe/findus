5b3e496faf1717a757a581349a110fe37d38184b
Nils Deppe <nils@nilsdeppe.com>
2025-09-17 13:03:32 -0700

    Add function to print system process ID for each rts process ID

---
+++ Rts/Detail/PrintProcessPids.cpp
@@ -0,0 +1,25 @@
+// Copyright, Nils Deppe, 2022-
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Rts/Detail/PrintProcessPids.hpp"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+namespace rts::detail {
+void print_process_pids(const int process_id) {
+  const char* env_enable_pid_print =
+      // NOLINTNEXTLINE(concurrency-mt-unsafe)
+      std::getenv("RTS_PRINT_PID");
+  if (env_enable_pid_print == nullptr) {
+    return;
+  }
+  const std::string output_info =
+      std::string{"   pid:"} + std::to_string(getpid()) +
+      " RTS pid:" + std::to_string(process_id) + "\n";
+  std::cout << output_info << std::flush;
+}
+}  // namespace rts::detail
