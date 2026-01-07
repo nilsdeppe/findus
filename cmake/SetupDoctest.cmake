2cedc46005b93e2fad086e3cd9a4418e11049dcc
Nils Deppe <nils@nilsdeppe.com>
2026-01-06 18:31:50 -0800

    Disable CMake warnings that arise in doctest

--- cmake/SetupDoctest.cmake
+++ cmake/SetupDoctest.cmake
@@ -9,6 +9,9 @@ find_package(doctest CONFIG QUIET)
 if (NOT doctest_FOUND AND FINDUS_FETCH_DOCTEST)
   set(DOCTEST_NO_INSTALL YES CACHE BOOL "Disable doctest install" FORCE)
   include(FetchContent)
+  # Work around a CMake warning.
+  set(CMAKE_WARN_DEPRECATED_SAVE ${CMAKE_WARN_DEPRECATED})
+  set(CMAKE_WARN_DEPRECATED OFF CACHE BOOL "" FORCE)
   FetchContent_Declare(
     doctest
     GIT_REPOSITORY "https://github.com/doctest/doctest"
@@ -17,14 +20,19 @@ if (NOT doctest_FOUND AND FINDUS_FETCH_DOCTEST)
     ${FINDUS_FETCHCONTENT_BASE_ARGS}
   )
   FetchContent_MakeAvailable(doctest)
+  set(CMAKE_WARN_DEPRECATED ${CMAKE_WARN_DEPRECATED_SAVE} CACHE BOOL "" FORCE)
 endif()
 
 if (TARGET doctest::doctest)
+  # Work around a CMake warning.
+  set(CMAKE_WARN_DEPRECATED_SAVE ${CMAKE_WARN_DEPRECATED})
+  set(CMAKE_WARN_DEPRECATED OFF CACHE BOOL "" FORCE)
   # Include cmake config from doctest
   include(${doctest_SOURCE_DIR}/scripts/cmake/doctest.cmake)
   # We need to use a hack to deal with static libs.
   # This hack is even provided by doctest.
   include(${doctest_SOURCE_DIR}/examples/exe_with_static_libs/doctest_force_link_static_lib_in_target.cmake)
+  set(CMAKE_WARN_DEPRECATED ${CMAKE_WARN_DEPRECATED_SAVE} CACHE BOOL "" FORCE)
 else(FINDUS_FETCH_DOCTEST)
   message(STATUS
     "doctest not found. Set -DFINDUS_FETCH_DOCTEST=ON if you want to "
