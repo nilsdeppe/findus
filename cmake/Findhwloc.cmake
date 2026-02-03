c5fe4ce2ec52a766924baecb4a1273b374236faf
Nils Deppe <nils@nilsdeppe.com>
2026-02-02 19:25:36 -0800

    Find hwloc more cleanly

--- cmake/Findhwloc.cmake
+++ cmake/Findhwloc.cmake
@@ -2,38 +2,11 @@
 # Distributed under the MIT License.
 # See LICENSE.txt for details.
 
-find_path(
-  hwloc_INCLUDE_DIRS
-  PATH_SUFFIXES include
-  NAMES hwloc.h
-  HINTS ${hwloc_DIR} ${hwloc_ROOT} $ENV{hwloc_DIR} $ENV{hwloc_ROOT}
-  DOC "hwloc include directory. Use hwloc_ROOT or hwloc_DIR to set search dir"
-)
+# Findhwloc.cmake
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(hwloc REQUIRED IMPORTED_TARGET hwloc)
 
-find_library(
-  hwloc_LIBRARIES
-  NAMES hwloc.a hwloc libhwloc
-  HINTS ${hwloc_DIR} ${hwloc_ROOT} $ENV{hwloc_DIR} $ENV{hwloc_ROOT}
-  PATH_SUFFIXES lib lib64
-  DOC "hwloc library. Use hwloc_ROOT or hwloc_DIR to set search dir"
-)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  hwloc
-  FOUND_VAR hwloc_FOUND
-  REQUIRED_VARS hwloc_LIBRARIES hwloc_INCLUDE_DIRS
-)
-mark_as_advanced(
-  hwloc_LIBRARIES hwloc_INCLUDE_DIRS
-)
-
-if (NOT hwloc_FOUND)
-  return()
+# Create an alias so consumers can use hwloc::hwloc
+if(TARGET PkgConfig::hwloc AND NOT TARGET hwloc::hwloc)
+    add_library(hwloc::hwloc ALIAS PkgConfig::hwloc)
 endif()
-
-add_library(hwloc::hwloc INTERFACE IMPORTED)
-set_property(TARGET hwloc::hwloc
-  PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${hwloc_INCLUDE_DIRS})
-set_property(TARGET hwloc::hwloc
-  PROPERTY INTERFACE_LINK_LIBRARIES ${hwloc_LIBRARIES})
